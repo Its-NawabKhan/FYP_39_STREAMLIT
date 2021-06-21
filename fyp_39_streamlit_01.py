25b028e49bb06f8e06304f97f620220b33175b72
Anzah Mehmood <anzah.mehmood@gmail.com>
2021-06-22 02:39:08 +0500

    Add files via upload

--- fyp_39_streamlit_01.py
+++ fyp_39_streamlit_01.py
@@ -3,12 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df_data= pd.read_csv('df_data.csv')
+df_data= pd.read_csv('D:/backup/Work_Extra/nawabkhan_fyp/time_generated.csv')
 # Get the data from url and request it as json file
 
 df_data01 = df_data.copy()
-
-df_data01.columns
 
 df_data01.drop(columns=['Unnamed: 0', 'Unnamed: 0.1', 'arrival', 'parking_entry', 'kiosk',
        'department_entrance', 'department_exit', 'parking_exit', 'departure'], inplace=True)
@@ -23,16 +21,12 @@ df_diff = pd.DataFrame(columns=df_data01.columns)
 
 df_diff['Date'] = df_aggreg['Date']
 
-df_diff
-
 for each in df_diff.columns[0:-1]:
     df_diff[each] = df_aggreg[each] - means_dict[each]
 
 df_diff.sort_values('Date', ascending=False, inplace=True)
 df_diff.set_index('Date', inplace=True)
 df_diff.reset_index(inplace=True)
-
-df_diff.head(7)
 
 @st.cache(persist=True, suppress_st_warning=True)
 
@@ -48,10 +42,10 @@ if radio_val== 'pie chart':
     st.write(df_data.head(5))
 
 elif radio_val== 'bar graph':
-    x=['a','b','c']
-    vals=[10,5,15]
+    # x=['a','b','c']
+    # vals=[10,5,15]
     fig = plt.figure()
-    sns.barplot(x,vals)
+    sns.barplot(df_diff['Date'],df_diff['kiosk_wait'])
     st.pyplot(fig)
 else:
     st.write(df_data.head(15))
